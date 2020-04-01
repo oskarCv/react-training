b1ca89bb3dde2a9d98d2b5d84bd7823d894f6f5f
System Administrator <root@darkozCv.local>
2020-04-01 11:59:58 -0600

    adding card container and card components

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,34 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {CardList} from './components/card-list/card-list.component';
 import './App.css';
 
 class App extends Component {
   constructor(){
     super();
     this.state={
-      monsters:[{
-        name:'Frank',
-        id:'mF'
-      },{
-        name: 'Dracula',
-        id:'mD'
-      },{
-        name:'Mummy',
-        id:'mM'
-      },{
-        name:'Wolf',
-        id:'mW'
-      }
-    ]};
+      monsters:[]
+    };
+  }
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters:users}));
   }
   render(){
     return (
       <div className="App">
-        {
-          this.state.monsters.map((monster)=>(
-            <h1>{monster.name}</h1>
-          ))
-        }
+        <CardList monsters={this.state.monsters} />
       </div>
     );
   }
