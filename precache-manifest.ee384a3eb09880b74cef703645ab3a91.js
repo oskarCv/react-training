991a08cac1c425dac8e7c48e95406478d2a81edf
System Administrator <root@darkozCv.local>
2020-04-01 18:33:30 -0600

    Updates

---
+++ precache-manifest.ee384a3eb09880b74cef703645ab3a91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "199ea88075832100d73dbd52efedede9",
+    "url": "/react-training/index.html"
+  },
+  {
+    "revision": "69aa76b57cf04ba57eab",
+    "url": "/react-training/static/css/main.5d87e6c3.chunk.css"
+  },
+  {
+    "revision": "147f1327e7798733efc4",
+    "url": "/react-training/static/js/2.d562cfe4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-training/static/js/2.d562cfe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69aa76b57cf04ba57eab",
+    "url": "/react-training/static/js/main.baf630e2.chunk.js"
+  },
+  {
+    "revision": "a626f626ae155806d97f",
+    "url": "/react-training/static/js/runtime-main.d72b4e67.js"
+  }
+]);
